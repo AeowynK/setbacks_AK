5bcee52c9a89050a65953d50077a19b293494942
Matt Davis <matt.davis@unh.edu>
2022-09-13 14:41:17 -0400

    some cleanup and add time graph

--- GroundLoop_superposition.py
+++ GroundLoop_superposition.py
@@ -286,7 +286,7 @@ def log_interp(zz, xx, yy):
     return np.power(10.0, np.interp(logz, logx, logy))
 
 
-def glhe_groundwater_model(params, x_locs, y_locs, times):
+def glhe_groundwater_model(params):
 
     """
     Calculates thermal response at some specified location (usually borehole
@@ -307,17 +307,15 @@ def glhe_groundwater_model(params, x_locs, y_locs, times):
 
     x_row = []
     y_row = []
-    g=[]
-    delT_loc = []
-    times = range[0, 1.578*(10**9)] #50 years span in seconds 
+    g = []
+    #create an array of times using logarithms
+    times = np.linspace(1, 10**9, num=50)
     load = 7.0 # degree to which load is unbalanced
     B = 6.0   # in meters; CSA standard is 3m to property lines
     
     x_locs = [-3*B, -2*B, -1*B, -3*B, -2*B, -1*B, -3*B, -2*B, -1*B]
     y_locs = [-B, -B, -B, 0, 0, 0, B, B, B]
 
-
-            
     for t in times:
 
         delT = []  # create an empty array to store the delT values 
@@ -340,9 +338,10 @@ def glhe_groundwater_model(params, x_locs, y_locs, times):
         #Calculate the total drawdown at the origin (x=0, y=0) 
         temp = s*load    # multiply the sum by the load to get the change in temp
         #store values at each location for times in loop.... call this g(t)
-        g = g.append(temp)       
+
+        g.append(temp)
         
-    return (times, g)
+    return times, g
 
 
 if __name__ == "__main__":
@@ -350,12 +349,14 @@ if __name__ == "__main__":
         params = Data(gw=5.e-16, k=1.5, ps=2650, cs=880, pw=1016, cw=3850, n=.1, to=0, H=100, phi=0.)
         
         # Call funtion so that 'result' is what is 'returned'.  In this case, two arrays, one with times the other with drawdowns
-        result = glhe_groundwater_model(params, x_locs, y_locs, delT)
+        times, g = glhe_groundwater_model(params)
         
-        #print(result)
+        plt.plot(np.asarray(times)/(86400*365),np.asarray(g))
+        plt.xlabel('time [years]')
+        plt.ylabel('thermal drawdown [C]')
+        plt.show()
     
    
 
-##  want to get Q/k
 
 
